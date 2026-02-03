e8a2f6051ee9e041aadbd1f4273034210ab20f20
Werner Eliaser <werner.eliaser@adaire.com>
2026-02-03 14:17:03 +0200

    db creds

--- src/shared/db/config.ts
+++ src/shared/db/config.ts
@@ -4,13 +4,7 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDc3RBcNEkLH59orv9iZRX2l9vSKIl4Usg",
-  authDomain: "find-it-namibia.firebaseapp.com",
-  projectId: "find-it-namibia",
-  storageBucket: "find-it-namibia.appspot.com",
-  messagingSenderId: "234023637323",
-  appId: "1:234023637323:web:7f2e242490e929504ed8ce",
-  measurementId: "G-3HT3WZP9KF"
+
 };
 
 const app = initializeApp(firebaseConfig);
